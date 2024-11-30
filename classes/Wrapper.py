e894042b4f337b607391ced2600a5e0c948e390f
pyTimK <timkristianllanto.tk@gmail.com>
2024-11-30 11:05:56 +0800

    .

--- classes/Wrapper.py
+++ classes/Wrapper.py
@@ -1,4 +1,4 @@
-from typing import Callable, Tuple
+from typing import Callable, List, Optional, Tuple
 from pynput.keyboard import Listener, Key
 
 from classes.Rich import Rich
@@ -8,16 +8,20 @@ class Wrapper:
     def __init__(
         self,
         loop: Callable,
-        keyboardEvents: Tuple[str, Callable],
+        keyboardEvents: List[Tuple[str, Callable]] = [],
+        onExit: Optional[Callable] = None,
         stop_key="q",
     ) -> None:
         self.loop = loop
         self.keyboardEvents = keyboardEvents
+        self.onExit = onExit
+
         self.stop_key = stop_key
 
         self.looping = True
 
         self.listener = Listener(on_press=self.on_press)
+
         self.listener.start()
 
         self.start()
@@ -28,6 +32,7 @@ class Wrapper:
     def on_press(self, key):
         if hasattr(key, "char") and key.char == self.stop_key:
             self.stop()
+            return
 
         for event in self.keyboardEvents:
             if hasattr(key, "char") and key.char == event[0]:
@@ -37,6 +42,7 @@ class Wrapper:
         while self.looping:
             self.loop()
 
+        self.onExit()
         self.listener.stop()
 
         Rich.printm("Exiting...")
